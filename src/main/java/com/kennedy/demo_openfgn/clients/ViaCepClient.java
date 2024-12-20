aeaed09d11ea3c8faf96da4ae010f2e028c5bbb9
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-20 14:20:17 -0300

    Created client

--- src/main/java/com/kennedy/demo_openfgn/clients/ViaCepClient.java
+++ src/main/java/com/kennedy/demo_openfgn/clients/ViaCepClient.java
@@ -3,9 +3,17 @@ package com.kennedy.demo_openfgn.clients;
 
 import com.kennedy.demo_openfgn.entities.CpfResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @FeignClient(name = "viaCpf", url = "https://viacep.com.br/ws")
 public interface ViaCepClient {
 
-//    public CpfResponse get
+    @GetMapping("/{cep}/json")
+    CpfResponse searchLocationByCep(@PathVariable String cep);
+
+    @GetMapping("/{uf}/{cidade}/{logradouro}/json")
+    List<CpfResponse> searchCepByAddress(@PathVariable String uf, @PathVariable String cidade, @PathVariable String logradouro);
 }
